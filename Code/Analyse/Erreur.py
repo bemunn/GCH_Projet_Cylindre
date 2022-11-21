32c3fff350f173a12591ff3c0e4592c6d8ac4cfe
bemunn <benjamin.munger@polymtl.ca>
2022-11-21 12:38:54 -0500

    Analyse erreur Nr Ntheta

--- Code/Analyse/Erreur.py
+++ Code/Analyse/Erreur.py
@@ -29,3 +29,32 @@ class parametres():
 
 prm = parametres()
 
+
+
+
+nr = np.arange(11,62,10)
+erreur_moy = np.zeros(len(nr))
+
+
+
+for a,n in enumerate(nr):
+    r,theta = position([prm.R, prm.R_ext],[0. , 2 * np.pi],n,n)
+    psi_num = mdf_assemblage(n,n, prm)
+    psi_exact = np.zeros(n**2)
+    print(n)
+    for i in range(n):
+        for j in range(n):
+            k = ij2k(i,j,n)
+            psi_exact[k] = prm.U_inf * r[-1-j,i] * np.sin(theta[-1-j,i]) * (1 - prm.R**2 / r[-1-j,i]**2)
+            
+    erreur_moy[a] = np.mean(abs(psi_exact - psi_num))
+    
+    
+ordre = np.log10(abs(erreur_moy[-1]-erreur_moy[0]))/np.log10(abs(nr[-1]-nr[0]))  
+plt.loglog(nr,erreur_moy,'-r')
+plt.title(r'Erreur sur la valeur de $\psi$ selon le nombre de points ($n_r$ = $n_{\theta}$)')
+plt.xlabel('N')
+plt.ylabel('Erreur')
+plt.grid(which='both')
+plt.savefig('ErreurNrNtheta.png', dpi= 1000)
+plt.show()
